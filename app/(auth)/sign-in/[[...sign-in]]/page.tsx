652b2f175f39037938be3004b5119e01fea89ee6
xusnaa <xusnaya@gmail.com>
2025-05-21 01:25:28 +0200

    middleware issues

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SignedIn } from '@clerk/nextjs';
-import React from 'react';
-
-const SignIn = () => {
-  return <div className="text-slate-600"></div>;
-};
-
-export default SignIn;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Signup = () => {
-  return (
-    <div className="bg-gradient-to-r h-screen w-screen from-black to-blue-950 flex items-center justify-center"></div>
-  );
-};
-
-export default Signup;
